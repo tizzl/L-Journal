906f97f244852a95fef71d2493f6ed8eba14e200
tizzl <timohahn831@gmail.com>
2025-10-09 14:52:57 +0200

    LOG eingebaut

--- src/main/java/com/timo/Learning_Journal/service/SessionService.java
+++ src/main/java/com/timo/Learning_Journal/service/SessionService.java
@@ -5,6 +5,8 @@ import com.timo.Learning_Journal.entity.Session;
 import com.timo.Learning_Journal.repositories.SessionRepository;
 import jakarta.servlet.http.Cookie;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -13,10 +15,23 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class SessionService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(SessionService.class);
 
+    public void loginUser(Person person){
+        LOG.info("Nutzer {} hat sich eingelogged!", person.getName());
+    }
+
+    public void logoutUser(Person person){
+        LOG.info("Nutzer {} hat sich ausgelogged!", person.getName());
+    }
     private final SessionRepository sessionRepository;
 
     public Cookie createCookieSession(Session session) {
+
+        if (session == null || session.getId() == null) {
+            LOG.error("Fehler beim Erstellen des Cookies: Session oder ID ist null!");
+            throw new IllegalArgumentException("Ungültige Session");
+        }
 
         Cookie cookie = new Cookie("session-id", Long.toString(session.getId()));
         cookie.setPath("/");
@@ -29,11 +44,19 @@ public class SessionService {
     public Session createSession(Person person) {
         Session session = new Session();
         session.setPerson(person);
-        sessionRepository.save(session);
-        return session;
+        Session savedSession = sessionRepository.save(session);
+        LOG.info("Neue Session {} für Nutzer {} erstellt", savedSession.getId(), person.getName());
+        return savedSession;
     }
 
     public Cookie endCookieSession(Session session) {
+
+        try {
+            sessionRepository.deleteById(session.getId());
+            LOG.info("Session {} erfolgreich beendet", session.getId());
+        } catch (Exception e) {
+            LOG.error("Fehler beim Beenden der Session {}: {}", session.getId(), e.getMessage());
+        }
 
         sessionRepository.deleteById(session.getId());
         Cookie cookie = new Cookie("session-id", null);
